1cf692d097d46dbf5df052e53ecebd76ae868c82
Miguel <maemb1@outlook.com>
2023-06-02 22:44:37 -0500

    Mod2

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ComponentsModule } from '../../components/components.module';
+
+
+//crear una interface para definir propiedades que el componente lista va a tener
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  //crear nuevo listado
+  //componentes: any[] = [];
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
