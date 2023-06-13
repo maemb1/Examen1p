9496e98a503b49fdc462f53954bed122eaa1366b
Miguel <maemb1@outlook.com>
2023-06-12 19:12:11 -0500

    Leccion

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
+
   }
 
-}
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: [
+        {
+          text: 'OK!',
+          handler: () => {
+            console.log('Click en OK!')
+          }
+        },
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Click en Cancelar')
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'rojo'
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async presentAlertMultilpeButton() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['Cancel', 'Opne Modal', 'Delete']
+    });
+    await alert.present();
+  };
+} 
